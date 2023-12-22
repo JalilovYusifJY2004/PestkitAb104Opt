e29ea735a811a2b6a6ca16dd4261356dd219c515
esli <tu7MXGB4G@code.edu.az>
2023-12-22 13:07:45 +0400

    add

---
+++ src/Core/PestKitOnionAB104.Application/Abstractions/Services/IAuthorService.cs
@@ -0,0 +1,14 @@
+﻿using PestKitOnionAB104.Application.DTOs.Author;
+
+namespace PestKitOnionAB104.Application.Abstractions.Services
+{
+    public interface IAuthorService
+    {
+        Task<ICollection<AuthorItemDto>> GetAllAsync(int page, int take);
+        //Task<GetCategoryDto> GetAsync(int id);
+        Task CreateAsync(AuthorCreateDto authorCreateDto);
+        Task UpdateAsync(int id, AuthorUpdateDto authorUpdateDto);
+        Task DeleteAsync(int id);
+        Task SoftDeleteAsync(int id);
+    }
+}
---
+++ src/Core/PestKitOnionAB104.Application/ServiceRegistration/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+using PestKitOnionAB104.Application.Validators;
+using System.Reflection;
+
+namespace PestKitOnionAB104.Application.ServiceRegistration
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            //services.AddAutoMapper(typeof(CategoryProfile));
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddFluentValidation(options => options.RegisterValidatorsFromAssemblyContaining(typeof(DepartmentCreateDtoValidator)));
+
+            return services;
+        }
+    }
+}
